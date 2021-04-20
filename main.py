24904891446d1e42b653b9d78c2a4a8d9f4e3f53
PHBMB <58255326+PHBMB@users.noreply.github.com>
2021-04-20 20:02:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import numpy as np
 from ipywidgets import interact, interactive, fixed, interact_manual
 
 # Import clean data 
-path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/module_5_auto.csv'
+path = 'module_5_auto.csv'
 df = pd.read_csv(path)
-
-df.to_csv('module_5_auto.csv')
 
 # Let's get an understanding of the data
 df=df._get_numeric_data()
